0dfb824bc6fe1a3aad0351b7208895439e9c7f09
MagneticExcalibur <43622451+MagneticExcalibur@users.noreply.github.com>
2018-09-26 17:14:35 -0400

    Add files via upload
    First submission.

---
+++ P2T1_SalesPrediction_JasonParsons.py
@@ -0,0 +1,8 @@
+# Display percentage of profits
+# 9/26/2018
+# CTI-110 P2T1
+# Jason Parsons
+
+Total = float(input("Please enter the total annual sales for the company "))
+Profit = Total * .23
+print("The company has made a profit of $" + format(Profit, ',.2f') + ".")
